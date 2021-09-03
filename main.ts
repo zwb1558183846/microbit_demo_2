32fee09a8729f78ddc12736d9f80d616dca6184c
zwb1558183846 <1558183846@qq.com>
2021-09-03 18:57:05 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+basic.showIcon(IconNames.TShirt)
 basic.forever(function () {
-	
+    // 控制小车前方左右两侧探照灯的
+    cbit_小车类.RGB_Car_Big2(cbit_小车类.enColor.Red)
+    basic.pause(500)
+    cbit_小车类.RGB_Car_Big2(cbit_小车类.enColor.OFF)
+    basic.pause(500)
 })
